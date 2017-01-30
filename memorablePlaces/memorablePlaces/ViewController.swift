568e2275f11e41e9c8b9599b1999ad45531ad8bf
William Castellano <wcastellano13@gmail.com>
2017-01-29 23:34:32 -0500

    added the ability to delet rows from my list of places in the table

--- memorablePlaces/memorablePlaces/ViewController.swift
+++ memorablePlaces/memorablePlaces/ViewController.swift
@@ -115,7 +115,6 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     self.map.setRegion(region, animated: true)
   }
   
-  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
